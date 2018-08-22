271d612411ed3a212a053b003e90d7d416dce876
SoumyaKB96 <42297139+SoumyaKB96@users.noreply.github.com>
2018-08-22 17:13:41 +0530

    Add files via upload

---
+++ Q7.java
@@ -0,0 +1,14 @@
+class Q7
+{
+    public void main()
+    {int c=4;
+       for(int i=10;i>0;i++)
+       {
+           for(int j=2;j<i;j++)
+           if(i%j==0){c++;if(c==100001)System.out.println(i);break;}
+           
+           
+        
+    }
+}
+}

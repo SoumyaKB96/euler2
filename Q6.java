3ae1d023e17383204eb38c763177e75192ef5f35
SoumyaKB96 <42297139+SoumyaKB96@users.noreply.github.com>
2018-08-22 17:11:08 +0530

    Add files via upload

---
+++ Q6.java
@@ -0,0 +1,14 @@
+class Q6
+{
+    public void main()
+    {int s1=0,s2=0,diff=0;
+        for(int i=1;i<=100;i++)
+        {
+            s1+=(i*i);
+            s2+=i;
+        }
+        s2=s2*s2;
+        diff=s2-s1;
+        System.out.println(diff);
+    }
+}

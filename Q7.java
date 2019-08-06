d0d028ce36f56801eb52243a5a8cd30bae4f9b39
SoumyaKB96 <42297139+SoumyaKB96@users.noreply.github.com>
2019-08-06 17:07:13 +0530

    Update Q7.java

--- Q7.java
+++ Q7.java
@@ -1,14 +1,20 @@
-class Q7
-{
-    public void main()
-    {int c=4;
-       for(int i=10;i>0;i++)
-       {
-           for(int j=2;j<i;j++)
-           if(i%j==0){c++;if(c==100001)System.out.println(i);break;}
-           
-           
-        
-    }
-}
+public class Q7 {
+
+	public static void main(String[] args)
+	{
+		int c=6,i=13;
+		while(c<10001)
+		{
+			if(isprime(i++)==true)c++;
+		}
+        System.out.println(i);
+	}
+	public static boolean isprime(int n)
+	{
+		for(int i=2;i<n/2;i++)
+		{
+			if(n%i==0)return false;
+		}
+		return true;
+	}
 }

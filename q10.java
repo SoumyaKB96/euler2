77ff52c9c8d6f93dbfcbeb8788962c54a3e625af
SoumyaKB96 <42297139+SoumyaKB96@users.noreply.github.com>
2019-08-30 22:48:51 +0530

    Update q10.java

--- q10.java
+++ q10.java
@@ -15,7 +15,7 @@ public class q10 {
 	{
         if (n < 2) return false;
         else if (n == 2) return true;
-        for (int i = 2; i<Math.sqrt(n)+1;i++)
+        for (int i = 2; i<=Math.sqrt(n);i++)
             if (n % i == 0)
                 return false;
         return true;

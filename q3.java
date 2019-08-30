8bd8c6f9c45c94169ac778b8e6550439ca6155a1
SoumyaKB96 <42297139+SoumyaKB96@users.noreply.github.com>
2019-08-30 22:47:41 +0530

    Update q3.java

--- q3.java
+++ q3.java
@@ -22,7 +22,7 @@ public class q3 {
 	{
 		if (n < 2) return false;
         else if (n == 2) return true;
-		for(long i=2;i<Math.sqrt(n)+1;i++)
+		for(long i=2;i<Math.sqrt(n);i++)
 			{if(n%i==0)return false;}
 			return true;
 	}

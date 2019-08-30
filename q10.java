81810a33185f090fcbe73f44a3c375e83e1c3305
SoumyaKB96 <42297139+SoumyaKB96@users.noreply.github.com>
2019-08-30 22:43:48 +0530

    Update q10.java

--- q10.java
+++ q10.java
@@ -11,10 +11,11 @@ public class q10 {
 	        System.out.print(sum);
 
 	}
-	public static boolean isPrime(long n) {
+	public static boolean isPrime(long n)
+	{
         if (n < 2) return false;
         else if (n == 2) return true;
-        for (int i = 2; i<Math.pow(n,0.5)+1;i++)
+        for (int i = 2; i<Math.sqrt(n)+1;i++)
             if (n % i == 0)
                 return false;
         return true;

e8bbde7057bfa554d97903791e432c215aa45c79
SoumyaKB96 <42297139+SoumyaKB96@users.noreply.github.com>
2019-08-31 18:38:00 +0530

    Add files via upload

--- q7.java
+++ q7.java
@@ -5,22 +5,24 @@ public class q7 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int c=6,i=13;
-		while(c<10001)
+		while(c<=10001)
 		{
 			if(isprime(i++)==true)c++;
 		}
+		i--;
         System.out.println(i);
 	}
 	public static boolean isprime(int n)
 	{
-		 if (n < 2) return false;
-        else if (n == 2) return true;
-		for(int i=2;i<=Math.sqrt(n);i++)
-		{
-			if(n%i==0)return false;
-		}
-		return true;
-
+		if (n<=1) return false; 
+        if (n<=3) return true; 
+        if (n%2==0 || n%3==0) return false; 
+      
+        for (int i= 5; i*i<=n; i=i+6) 
+            if (n%i==0 || n%(i+2)==0) 
+            return false; 
+      
+        return true; 
 	}
 
 }

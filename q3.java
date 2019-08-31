078293b88069f071e0a615a45be35240d3817950
SoumyaKB96 <42297139+SoumyaKB96@users.noreply.github.com>
2019-08-31 18:28:59 +0530

    Add files via upload

--- q3.java
+++ q3.java
@@ -1,31 +1,28 @@
 package eu;
 
 public class q3 {
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		long numm = 600851475143l;
 		
-		long max=0;
-		
-		for (long i = 2; i<numm/2; i++) {
-		    if (numm%i==0 && isPrime(i))
-		    {max=i;}
-		    
-		    		}
-		
-		
-		 System.out.println(max);
-		
+        long n = 600851475143l;
+        long f = n;
+        long i = 2;
+        
+        while (i<=n && n!=1) 
+        {
+            if (n%i==0) 
+            {
+                n=n/i;
+                f=i;
+               
+
+            }
+            else  i++;
+            
+        }
+        System.out.println(f);
+
 	}
-	static boolean isPrime(long n)
-	{
-		if (n < 2) return false;
-        else if (n == 2) return true;
-		for(long i=2;i<=Math.sqrt(n);i++)
-			{if(n%i==0)return false;}
-			return true;
-	}
-	
+
 }
-		     

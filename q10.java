af6c5c57bacb5097b07336e5b6a82239c7faf3c3
SoumyaKB96 <42297139+SoumyaKB96@users.noreply.github.com>
2019-08-31 19:13:45 +0530

    Add files via upload

--- q10.java
+++ q10.java
@@ -4,21 +4,26 @@ public class q10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 long sum = 0;
-	        for (int i = 0; i < 2000000; i++)
-	            if (isPrime(i))
-	                sum += i;
-	        System.out.print(sum);
+		
+		int n = 2000000;
+		 long s = 0;
+	    boolean[] ar = new boolean[n];
+	    
+	    for (int i=2; i<n;i++)ar[i] = true;
+	    
+	    for (int i=2; i<=(int)Math.sqrt(n); i++)
+	    { 
+	        if (ar[i]==true)  for (int j=2; j*i<n; j++)ar[i*j]=false;
+	        
+	    }
+	    
+	   
+	    for (int i=2; i<n; i++) 
+	    { 
+	        if (ar[i]==true) s+=i;
+	    }
+	    System.out.println(s);
 
 	}
-	public static boolean isPrime(long n)
-	{
-        if (n < 2) return false;
-        else if (n == 2) return true;
-        for (int i = 2; i<=Math.sqrt(n);i++)
-            if (n % i == 0)
-                return false;
-        return true;
-    }
 
 }

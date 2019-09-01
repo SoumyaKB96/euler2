432e4ad39b2cdfb24ac64ae858f8cb69d8d2b723
SoumyaKB96 <42297139+SoumyaKB96@users.noreply.github.com>
2019-09-01 13:50:26 +0530

    Add files via upload

--- q10.java
+++ q10.java
@@ -4,26 +4,22 @@ public class q10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int n = 2000000;
-		 long s = 2;
-	    boolean[] ar = new boolean[n];
-	    
-	    for (int i=3; i<n;i+=2)ar[i] = true;
-	    
-	    for (int i=2; i<=(int)Math.sqrt(n); i++)
-	    { 
-	    	if (ar[i]==true) for (int j=2; j*i<n; j++)ar[i*j]=false;
-	        
-	    }
-	    
-	   
-	    for (int i=3; i<n; i+=2) 
-	    { 
-	        if (ar[i]==true) s+=i;
-	    }
-	    System.out.println(s);
-
+		long s=2;
+		for(long i=3;i<2000000;i+=2)
+		{
+			int k=0;
+			for (int j=3; j<=(int)Math.sqrt(i); j++)
+		    { 
+		    	if(i%j==0)
+				{
+					k=1;
+					break;
+				}
+		    }
+				
+			if(k==0) s+=i;
+		}
+		System.out.println(s);
 	}
 
 }
